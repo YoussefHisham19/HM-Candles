a4dd0beb7de0d26ffce0c253537d20635d6d6b14
YoussefHisham19 <youssefhisham375@gmail.com>
2025-08-24 22:06:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const filterBtns =document.querySelectorAll('button[data-filter]')
+const products =document.querySelectorAll('.product')
+filterBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const filter = btn.getAttribute("data-filter");
+
+    products.forEach(product => {
+      if (product.getAttribute("data-category") === filter || filter === "all") {
+        product.style.display = ""; // يظهر
+      } else {
+        product.style.display = "none";  // يختفي
+      }
+    });
+  });
+});
+
+
+const swiper = new Swiper('.swiper', {
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
